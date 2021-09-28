83409450426b767a72f58cc45f9abc13890c3471
Nishant Kathiriya <nishant.kathiriya@gmail.com>
2021-09-28 19:02:57 +0100

    used fontawesome icon for delete button

--- frontend/src/LeftPanel.js
+++ frontend/src/LeftPanel.js
@@ -2,21 +2,23 @@ import React from "react";
 import { make_post_request } from "./Utility";
 import $ from "jquery";
 import { Link } from "react-router-dom";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import * as icons from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import * as icons from "@fortawesome/free-solid-svg-icons";
 
 const LeftPanel = (props) => {
    const { list, get_list_from_server } = props;
    const add_list = async (e) => {
       if (e.key === "Enter") {
          const list_name = $("#list_name_input").val();
-         if (!list.includes(list_name) && list_name !== "") {
-            await make_post_request({
-               command: "add_list",
-               list_name: list_name,
-            });
-            await get_list_from_server();
-            $("#list_name_input").val("");
+         if (/^[0-9a-zA-Z_-]+$/.test(list_name)) {
+            if (!list.includes(list_name) && list_name !== "") {
+               await make_post_request({
+                  command: "add_list",
+                  list_name: list_name,
+               });
+               await get_list_from_server();
+               $("#list_name_input").val("");
+            }
          }
       }
    };
@@ -36,16 +38,17 @@ const LeftPanel = (props) => {
             <Link to={`/${item}`} key={index}>
                {item}
             </Link>
-            <FontAwesomeIcon icon={icons.faTrashAlt} onClick={() => delete_list_clicked(index)} />
+            <FontAwesomeIcon
+               icon={icons.faTrashAlt}
+               onClick={() => delete_list_clicked(index)}
+            />
          </li>
       );
    });
 
    return (
       <div className="admin-left">
-         <ul className="admin-listitem">
-            {list_render}
-         </ul>
+         <ul className="admin-listitem">{list_render}</ul>
          <br key="br" />
          <input
             key="list_name_input"
